d0f176161e55df0fb2d31388f4a4f8a117b88017
Terezia Kovalova <B2206426@student.burycollege.ac.uk>
2025-11-26 09:39:19 +0000

    Update Job.cs

--- Models/Job.cs
+++ Models/Job.cs
@@ -2,12 +2,13 @@
 {
     public class Job
     {
-        public string JobID { get; set; }       // from Jobs
-        public string ClientName { get; set; }  // FirstName + LastName
-        public string ScheduledDate { get; set; } // YYYY-MM-DD from Appointments
+        public string JobID { get; set; }     
+        public string ClientName { get; set; }  
+        public string ScheduledDate { get; set; }
         public string Status { get; set; }
         public string ServiceName { get; set; }
         public string Address { get; set; }
         public string Postcode { get; set; }
     }
 }
+
