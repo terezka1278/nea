5d2a63f96203723dcf0e6e8beda672b7e5912350
Terezia Kovalova <B2206426@student.burycollege.ac.uk>
2025-11-26 09:38:19 +0000

    Update ClientBooking.cs

--- Dtos/ClientBooking.cs
+++ Dtos/ClientBooking.cs
@@ -5,9 +5,9 @@
         public string JobID { get; set; } = "";
         public string ServiceName { get; set; } = "";
         public DateTime ScheduledDate { get; set; }
-        public bool TimeSlotPm { get; set; }  // matches Appointments.TimeSlot (BOOL)
-        public string Status { get; set; } = ""; // Jobs.Status (Not started / In progress / Completed)
-        public float? EstPrice { get; set; }  // from Quotes.EstPrice if exists
+        public bool TimeSlotPm { get; set; } 
+        public string Status { get; set; } = ""; 
+        public float? EstPrice { get; set; } 
     
     }
 
@@ -27,7 +27,7 @@
         public string ServiceName { get; set; } = "";
         public float EstPrice { get; set; }
         public float EstDuration { get; set; }
-        public int Accepted { get; set; }   // 0/1 from DB
+        public int Accepted { get; set; }  
     }
 
     public class ClientInvoice
@@ -40,4 +40,5 @@
         public float DepositPaid { get; set; }
     }
 }
+
 
