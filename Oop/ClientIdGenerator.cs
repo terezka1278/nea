5e8f3a86895629d4581aab614c0a4020a571cf8f
Terezia Kovalova <B2206426@student.burycollege.ac.uk>
2025-11-26 09:25:11 +0000

    Update ClientIdGenerator.cs comments done

--- Oop/ClientIdGenerator.cs
+++ Oop/ClientIdGenerator.cs
@@ -2,15 +2,13 @@
 {
     public static class ClientIdGenerator
     {
-        public static string CreateClientId(string firstName, string lastName, string phoneNum)
+        public static string CreateClientId(string firstName, string lastName, string phoneNum) //create a method to generate a clientID
         {
-           
+            string f = firstName.Substring(0, 1).ToUpper();  //first letter of first name in upper case
+            string l = lastName.Substring(0, 1).ToUpper(); //first letter of last name in upper case
+            string last3 = phoneNum.Substring(phoneNum.Length - 3); //last 3 numbers of phone number
 
-            string f = firstName.Substring(0, 1).ToUpper();
-            string l = lastName.Substring(0, 1).ToUpper();
-            string last3 = phoneNum.Substring(phoneNum.Length - 3);
-
-            return f + l + last3;
+            return f + l + last3; //return  generated clientID of first letter in first name, first letter in last name and last 3 numbers of phone number
         }
     }
 }
